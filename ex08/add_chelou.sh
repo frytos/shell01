50081bc6ed48230a2c35d666cd1c4eabbcf94c4e
Arnaud Groussard <argrouss@MADE-f0Br6s16.clusters.42paris.fr>
2024-02-08 09:36:40 +0100

    adds addition in base5

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "ibase=5;obase=D;$(echo $( echo "$(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234)" | bc ))" | bc | tr "0123456789ABC" "gtaio luSnemf"
+echo "ibase=5;obase=D;$(echo $( echo "ibase=5;obase=5;$(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234)" | bc ))" | bc | tr "0123456789ABC" "gtaio luSnemf"
 
