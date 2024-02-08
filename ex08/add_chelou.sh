fb27b1183b4c98d2ceb258c598b51c7576c43fce
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 04:01:17 +0100

    converts the base special char in base5 and the base mrdoc in base 5

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo $( expr 15 + 30 ) | tr "0123456789ABC" "gtaio luSnemf"
+echo $( expr $(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234) ) | tr "0123456789ABC" "gtaio luSnemf"
