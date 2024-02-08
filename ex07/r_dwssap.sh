2ecd8e958e85ce8e5be74f08427ff49dece06cf0
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 02:21:13 +0100

    removes all login after the FT_LINE2 th

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /etc/passwd | sed '/\#/d' | sed -n 'n;p' | awk -F: '{print $1}' | rev | sort -r
+cat /etc/passwd | sed '/\#/d' | sed -n 'n;p' | awk -F: '{print $1}' | rev | sort -r | head -n $FT_LINE2
