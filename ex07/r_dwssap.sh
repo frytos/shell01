18935439ce2fd3a37f7c6e6a9445383a27e20317
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 02:24:55 +0100

    replace the last ', ' by '.'

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /etc/passwd | sed '/\#/d' | sed -n 'n;p' | awk -F: '{print $1}' | rev | sort -r | head -n $FT_LINE2 | tail -n $( expr $FT_LINE2 - $FT_LINE1 + 1 ) | tr '\n' ',' | sed 's&,&, &g'
+cat /etc/passwd | sed '/\#/d' | sed -n 'n;p' | awk -F: '{print $1}' | rev | sort -r | head -n $FT_LINE2 | tail -n $( expr $FT_LINE2 - $FT_LINE1 + 1 ) | tr '\n' ',' | sed 's&,&, &g' | rev | sed 's/ ,/./' |  rev
