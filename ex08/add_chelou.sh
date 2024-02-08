bf5ddd88dfca91dceed4b25b00d20d591e9581ac
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 04:27:55 +0100

    resolve the sum for Salut, after adding the 2 base5 numbers, transforming them in base13 (baseD), sending it to bc, and translating it in base 'gtaio luSnemf'

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo $( expr $(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234) ) | tr "0123456789ABC" "gtaio luSnemf"
+echo "ibase=5;obase=D;$(echo $( expr $(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234) ))" | bc | tr "0123456789ABC" "gtaio luSnemf"
