c93e1a75b33e61f95ae0777cd8d2571c52ff61c7
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 05:33:28 +0100

    switched from expr to bc to handle large number limitations

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-echo "ibase=5;obase=D;$(echo $( expr $(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234) ))" | bc | tr "0123456789ABC" "gtaio luSnemf"
+echo "ibase=5;obase=D;$(echo $( echo "$(echo $FT_NBR1 | sed "s/'/0/g" | sed 's@\\@1@g' | sed 's/"/2/g' | sed 's/?/3/g' | sed 's/!/4/g') + $(echo $FT_NBR2 | tr mrdoc 01234)" | bc ))" | bc | tr "0123456789ABC" "gtaio luSnemf"
+
